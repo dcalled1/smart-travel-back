5c3ced6adb6795d9824ddb119dd0c9be4682b0de
dcalled1 <dcalled1@eafit.edu.co>
2022-05-18 07:50:53 -0500

    a

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from fuzzy import simulate, load_fuzzy_controller
 from sentiment import classify, load_model
 from operator import itemgetter
-from random import choice
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
